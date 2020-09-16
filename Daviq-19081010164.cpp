ee8e79a468f457e72b41ff23d3d5e10d5880a34a
unknown <you@example.com>
2020-09-16 12:49:58 +0700

    biodata

---
+++ Daviq-19081010164.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+	
+	printf("\tBIODATA PRIBADI\n\n");
+	printf("Nama         : Daviq Ardli Wishesha\n");
+	printf("NPM          : 19081010164\n");
+	printf("Tempat lahir : Bogor\n");
+	printf("tanggal lahir: 12 Juni 2001\n");
+	printf("Jurursan     : Informatika\n");
+	printf("Angkatan     : 2019");
+	return 0;
+}
